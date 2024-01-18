bb2ac5d23cecac54c176d7d39cbc4d1224f92f1b
Ido Admoni <ido.admoni@post.runi.ac.il>
2024-01-18 18:28:54 +0200

    HW4 done!

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,106 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+       testFindMissingInt();
+       testSecondMaxValue();
+       testContainsTheSameElements();
+       testIsSorted();
     }
 
+    public static void testFindMissingInt () {
+         int[] array = {3, 0, 1};
+        int result1 = findMissingInt(array);
+        System.out.println(result1);
+    }
+
+    public static void testSecondMaxValue () {
+       int[] array = {3, 0, 1};
+       int result2 = secondMaxValue(array);
+       System.out.println(result2);
+   }
+
+   public static void testContainsTheSameElements () {
+    int[] array1 = {-4, 5, 3, 1};
+    int[] array2 = {1, -4, 5};
+    boolean result = containsTheSameElements(array1, array2);
+    System.out.println("Contains: " + result);
+}
+
+public static void testIsSorted () {
+    int[] array = {1, 3, 2};
+    boolean result = isSorted(array);
+    System.out.println("Is sorted :" + result);
+}
+
+    public static int findMissingInt (int [] array) {
+        int n = array.length;
+        int expectedSum = 0;
+        // Calculate the sum of integers from 1 to n
+        for (int i = 0; i < n+1; i++) {
+            expectedSum += i;
+        }
+        // Calculate the sum of array elements
+        int actualSum = 0;
+        for (int i = 0; i < n; i++) {
+            actualSum = actualSum + array[i];
+        }
+        int missingInt = (expectedSum - actualSum);
+        return (missingInt);
+    }
+    
+
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int largest = 0;
+        int secondLargest = 0;
+        for (int i =0; i< array.length; i++){
+            if(array[i] > largest){
+                secondLargest = largest;
+                largest = array[i];
+            } else if (array[i] > secondLargest){
+                secondLargest = array[i];
+            }
+        }
+        return secondLargest;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int n1 = array1.length;
+        int n2= array2.length;
+        for (int i = 0; i < n1; i++) {
+            boolean found = false;
+            for (int j = 0; j < n2; j++) {
+                if (array1[i] == array2[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        for (int i = 0; i < n2; i++) {
+            boolean found = false;
+            for (int j = 0; j < n1; j++) {
+                if (array2[i] == array1[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        int n = array.length;
+        boolean ans = true;
+        for (int i = 0 ; i < n- 1; i++){
+            if (array[i+1] > array[i]) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
     }
-
-}
+    }
--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -22,21 +24,150 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String str="Hello    World Hi";
+        System.out.println(removeSpaces(str));
+        testCapVowelsLowRest();
+        testCamelCase()
+        testallIndexOf();
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+public static void testCapVowelsLowRest () {
+        String str = "One two tHRee world";
+        String result1 = capVowelsLowRest(str);
+        System.out.println("CapVowelsLowRest: " + result1);
+   } 
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+   public static String capVowelsLowRest (String string) {
+    String newStr = "";
+    for (int i = 0; i < string.length(); i++){
+        if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i'
+            || string.charAt(i) == 'o' || string.charAt(i) == 'u' || string.charAt(i) == 'A' 
+            ||string.charAt(i) == 'E' ||string.charAt(i) == 'I' ||string.charAt(i) == 'O' ||string.charAt(i) == 'U'){
+            int temp = string.charAt(i) - 32;
+            newStr = newStr + (char) temp; 
+        }
+        else if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+            int temp = string.charAt(i) + 32;
+            newStr = newStr + (char) temp; 
+        }
+        else {
+            newStr = newStr + string.charAt(i);
+        }
     }
+    
+        return newStr;
+}
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+public static String camelCase (String string) {
+    String str = "";
+    boolean isLastSpace = true;
+    int isFoundFirstWord = 0;
+    char ch = ' ';
+    for(int i = 0; i< string.length(); i++){
+        if(string.charAt(i) == ' '){
+            isLastSpace = true;
+        } else {
+            //processing the characters but spaces
+            if (isLastSpace == true) {
+                ///begining of word
+                isLastSpace = false;
+                // convert char to uppercase
+
+                switch (isFoundFirstWord){
+                    case 0:
+                        ch = convertLowerCase(string.charAt(i));
+                        isFoundFirstWord = 1;
+                    break;
+                    case 1:
+                        ch = convertUpperCase(string.charAt(i));
+                    break;
+                }                    
+            } else {
+                // convert char to lowcase
+                ch = convertLowerCase(string.charAt(i));
+            }
+            //add char to str
+            str = str + ch;
+        }                
+    }    
+    return str;
+}
+
+public static Boolean isUpperCase (char ch){
+       return (ch >= 'A'&& ch <= 'Z' );      
+}
+
+public static char convertUpperCase( char ch) {
+    if(isUpperCase(ch) == false){
+        return (char)(ch - 32);
+    } else {
+        return ch;
     }
 }
+
+public static char convertLowerCase( char ch) {
+    if(isUpperCase(ch) == true){
+        return (char)(ch + 32);
+    } else {
+        return ch;
+    }
+}
+
+public static void testcamelCase() {
+    String expecteString = "helloWorld";
+    String camelcase;
+    camelcase = camelCase("Hello World");
+    varifycamelCase(expecteString, camelcase);
+    expecteString = "helloWorld";
+    camelcase = camelCase("HELLO    world");
+    varifycamelCase(expecteString, camelcase);
+    expecteString = "introToComputerScience";
+    camelcase = camelCase("Intro to coMPUter sCIEncE");
+    varifycamelCase(expecteString, camelcase);
+
+}
+public static void varifycamelCase (String expected, String actual){
+        System.out.printf("The new string is %s \n",actual);
+}
+        public static void testallIndexOf() {
+            int expected [] = {2, 3, 9};
+            char characterToFind = 'l';
+            int[] allIndexOf = allIndexOf("Hello world",characterToFind);
+            varifyAllIndexOf(expected, allIndexOf);
+           
+    
+        }
+        public static void varifyAllIndexOf (int[] expected, int[] actual) {
+                System.out.printf("The new array is %s \n",Arrays.toString(actual));  
+        }
+        public static int[] allIndexOf (String string, char chr) {
+            int count = 0;
+            int arr[];
+            int index = 0;
+            for(int i = 0; i < string.length(); i++){
+               if(string.charAt(i) == chr){
+                count++;
+               }
+            }
+            arr = new int[count];
+            for(int i = 0; i < string.length(); i++){
+                if(string.charAt(i) == chr){
+                    arr[index] = i;
+                    index++;
+                }
+            }
+            return  arr;
+        }
+
+        public static void testallIndexOf() {
+            int expected [] = {2, 3, 9};
+            char characterToFind = 'l';
+            int[] allIndexOf = allIndexOf("Hello world",characterToFind);
+            varifyAllIndexOf(expected, allIndexOf);
+           
+    
+        }
+        public static void varifyAllIndexOf (int[] expected, int[] actual) {
+                System.out.printf("The new array is %s \n",Arrays.toString(actual));  
+        }
+    }
